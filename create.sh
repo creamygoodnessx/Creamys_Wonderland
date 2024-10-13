294e664eb48fc1026ff28c2309e70b931bc4cca1
creamygoodnessx <jrascot@hotmail.com>
2024-10-13 16:06:54 -0400

    add files

---
+++ create.sh
@@ -0,0 +1,7 @@
+echo "# Creamys_Wonderland" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/creamygoodnessx/Creamys_Wonderland.git
+git push -u origin main
